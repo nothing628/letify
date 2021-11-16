c16f66743e2b520ec1272f5822bd8552e3620725
nothing628 <yogyphang@gmail.com>
2021-11-16 21:48:14 +0700

    finish bottom cicle

--- lib/widgets/circle_progress.dart
+++ lib/widgets/circle_progress.dart
@@ -2,17 +2,42 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 class _MyCustomPainter extends CustomPainter {
+  double getStartAngle(double r, double c) {
+    double sinValue = (c * 0.5) / r;
+    double theta = asin(sinValue * 2);
+
+    return theta + pi / 2 + 0.1;
+  }
+
+  double getSweepAngle(double r, double c, double percent) {
+    double sinValue = (c * 0.5) / r;
+    double theta = asin(sinValue * 2);
+
+    return (theta * percent * -2.2);
+  }
+
   @override
   void paint(Canvas canvas, Size size) {
+    double r = size.width + 150;
+    double c = size.width;
+    double startAngle = getStartAngle(r, c);
+    double sweepAngle = getSweepAngle(r, c, 0.5);
+    double fullSweepAngle = getSweepAngle(r, c, 1);
+
     Rect myCanvasSize =
         Rect.fromLTRB(-75, 0, size.width + 75, size.width + 150);
     Paint myPaint = Paint();
 
-    myPaint.color = const Color.fromRGBO(195, 60, 232, 1);
+    // Draw empty line
     myPaint.style = PaintingStyle.stroke;
-    myPaint.strokeWidth = 6;
+    myPaint.strokeCap = StrokeCap.round;
+    myPaint.color = const Color.fromRGBO(36, 15, 81, .1);
+    myPaint.strokeWidth = 1;
+    canvas.drawArc(myCanvasSize, startAngle, fullSweepAngle, false, myPaint);
 
-    canvas.drawArc(myCanvasSize, 0, pi, false, myPaint);
+    myPaint.color = const Color.fromRGBO(195, 60, 232, 1);
+    myPaint.strokeWidth = 6;
+    canvas.drawArc(myCanvasSize, startAngle, sweepAngle, false, myPaint);
   }
 
   @override
